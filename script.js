75d5c2c043cc392eb007ea2dafa0ad7a94949981
djns <130430285+DDJCS50@users.noreply.github.com>
2023-05-27 12:19:00 -0700

    Complete playRound func
    Completed implementation of playRound so that the value returned is
    either win, lose, or tie based on the rules of rock, paper, scissors

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ function playRound(playerSelection, computerSelection) {
     let playerSelectionBody = playerSelection.substring(1);
     let playerSelectionFirst = playerSelection.charAt(0).toUpperCase();
     playerSelection = playerSelectionFirst + playerSelectionBody;
+
+    if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        return "You Lose! Rock beats Scissors";
+    } else if (computerSelection === "Rock" && playerSelection === "Paper") {
+        return "You Win! Paper beats Rock";
+    } else if (computerSelection === "Rock" && playerSelection === "Rock") {
+        return "Tie! Rock equals Rock";
+    } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        return "You Lose! Paper beats Rock";
+    } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
+        return "You Win! Scissors beats Paper";
+    } else if (computerSelection === "Paper" && playerSelection === "Paper") {
+        return "Tie! Paper equals Paper";
+    } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        return "You Lose! Scissors beats Paper";
+    } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
+        return "You Win! Rock beats Scissors";
+    } else if (computerSelection === "Scissors" && playerSelection === "Scissors") {
+        return "Tie! Scissors equals Scissors";
+    }
 }
 
 playRound("ScIsSoRs", getComputerChoice());
