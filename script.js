4adf3a9c22f76f57f4184be79f4a9934ce6fcc22
djns <130430285+DDJCS50@users.noreply.github.com>
2023-05-27 12:44:52 -0700

    Modify funcs
    Changed playRound func to include score count incrementation. Modified
    game func to play 5 rounds. Deleted unnecessary variable for the prompt

--- script.js
+++ script.js
@@ -20,24 +20,40 @@ function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelectionFirst + playerSelectionBody;
 
     if (computerSelection === "Rock" && playerSelection === "Scissors") {
+        computerScore++;
         return "You Lose! Rock beats Scissors";
     } else if (computerSelection === "Rock" && playerSelection === "Paper") {
+        playerScore++;
         return "You Win! Paper beats Rock";
     } else if (computerSelection === "Rock" && playerSelection === "Rock") {
         return "Tie! Rock equals Rock";
     } else if (computerSelection === "Paper" && playerSelection === "Rock") {
+        computerScore++;
         return "You Lose! Paper beats Rock";
     } else if (computerSelection === "Paper" && playerSelection === "Scissors") {
+        playerScore++;
         return "You Win! Scissors beats Paper";
     } else if (computerSelection === "Paper" && playerSelection === "Paper") {
         return "Tie! Paper equals Paper";
     } else if (computerSelection === "Scissors" && playerSelection === "Paper") {
+        computerScore++;
         return "You Lose! Scissors beats Paper";
     } else if (computerSelection === "Scissors" && playerSelection === "Rock") {
+        playerScore++;
         return "You Win! Rock beats Scissors";
     } else if (computerSelection === "Scissors" && playerSelection === "Scissors") {
         return "Tie! Scissors equals Scissors";
     }
 }
 
-playRound("ScIsSoRs", getComputerChoice());
+let playerScore = 0;
+let computerScore = 0;
+
+
+function game() {
+    for (let i = 0; i < 5; i++) {
+        playRound(prompt("Which sign would you like to choose? "), getComputerChoice());
+    }
+}
+
+game();
