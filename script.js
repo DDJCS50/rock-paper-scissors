347be17d3c4999a3ecf1a4eac69b730129c2480b
djns <130430285+DDJCS50@users.noreply.github.com>
2023-05-27 11:48:25 -0700

    Modify random function
    Completed implementation of the random function for the computer to
    select a random choice.

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 function getComputerChoice() {
-   randomNumber = () => Math.floor(Math.random() * 3);
+    let random;
+    function randomNumber(max) {
+        return Math.floor(Math.random() * max);
+    }
+    random = randomNumber(3);
+    if (random === 0) {
+        return "Rock";
+    } else if (random === 1) {
+        return "Paper";
+    } else {
+        return "scissors";
+    }
 }
+
+getComputerChoice();
